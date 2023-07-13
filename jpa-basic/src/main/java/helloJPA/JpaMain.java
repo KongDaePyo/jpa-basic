4f9c0a9245c3e3214160a27ededaa95a7aa16a8b
DESKTOP-JOJ5L1Q\82109 <c5ngdp@gmail.com>
2023-07-13 20:06:17 +0900

    Section 3 - 영속성 컨텍스트 1

--- jpa-basic/src/main/java/helloJPA/JpaMain.java
+++ jpa-basic/src/main/java/helloJPA/JpaMain.java
@@ -32,12 +32,25 @@ public class JpaMain {
 //            Member findMember = em.find(Member.class, 1L);
 //            findMember.setName("Kong2"); /* Update는 기존에 Insert 하듯이 setName으로 컬럼값을 주고 em.persist() 해야하는게 아닌 값만 바꿔도 Insert (Update) 해준다. */
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                            .getResultList(); /* JPQL 예시, JPA는 Table 대상으로 쿼리 X 객체를 대상으로 O */
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                            .getResultList(); /* JPQL 예시, JPA는 Table 대상으로 쿼리 X 객체를 대상으로 O */
+//
+//            for (Member member : result) {
+//                System.out.println(member.getName());
+//            }
 
-            for (Member member : result) {
-                System.out.println(member.getName());
-            }
+
+              /* 비영속 */
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("Kong");
+
+
+//            /* 영속 */
+//            System.out.println("before");
+//            em.persist(member); /* persist가 쿼리문을 날리는게 아니다. */
+//            System.out.println("after");
+
 
             tx.commit();
         } catch (Exception e) {
