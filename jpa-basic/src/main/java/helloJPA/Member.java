a1b7db6011de0828edf7d56e9c465731dca1f9c4
DESKTOP-JOJ5L1Q\82109 <c5ngdp@gmail.com>
2023-07-19 05:37:28 +0900

    Section 6 - 일대일

--- jpa-basic/src/main/java/helloJPA/Member.java
+++ jpa-basic/src/main/java/helloJPA/Member.java
@@ -48,7 +48,9 @@ public class Member {
 //    @JoinColumn(name = "TEAM_ID")
 //    private Team team;
 
-
+    @OneToOne /* 일대일 */
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 
     public Member() {} // JPA는 내부적으로 리플렉션 ? 같은 걸 사용하기 때문에 동적으로 객체를 생성해야함 -> 기본 생성자가 하나 있어야 한다.
 
