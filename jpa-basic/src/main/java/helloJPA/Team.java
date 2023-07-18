054316d9ef8afe7f6d8490a68bf2c2d49f8d3dce
DESKTOP-JOJ5L1Q\82109 <c5ngdp@gmail.com>
2023-07-18 11:10:39 +0900

    Section 5 - 단방향 연관관계

--- jpa-basic/src/main/java/helloJPA/Team.java
+++ jpa-basic/src/main/java/helloJPA/Team.java
@@ -17,5 +17,20 @@ public class Team {
 
     public Team() {
     }
-    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
